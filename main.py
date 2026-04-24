b9896b0b195f61c45b4680a7f93dcd22caf1f2b5
aytenokayi690-coder <aytenokayi690@gmail.com>
2026-04-24 03:28:00 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class JarvisApp(App):
-    def build(self):
-        return Label(text='Jarvis Sisteme Baglandi, Efendim!')
-
-if __name__ == '__main__':
-    JarvisApp().run()
